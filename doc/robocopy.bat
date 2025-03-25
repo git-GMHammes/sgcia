d03174484d69eed7ceef800c1f915403c4b2f59b
Gustavo Hammes <sistema.gustavo@gmail.com>
2025-03-25 17:01:05 -0300

    DEV

---
+++ doc/robocopy.bat
@@ -0,0 +1,4 @@
+robocopy "C:\laragon\www\sgcpro\src\public\script\react_modelo_v1\frontend" "C:\laragon\www\sgcia\src\public\script\react_modelo_v1\frontend" /MIR /XD "node_modules" "src_2"
+robocopy "C:\laragon\www\sgcpro\doc" "C:\laragon\www\sgcia\doc" /MIR /XD "node_modules" "src_2"
+robocopy "C:\laragon\www\sgcprosrc\app" "C:\laragon\www\sgciasrc\app" /MIR /XD "node_modules" "src_2"
+pause
